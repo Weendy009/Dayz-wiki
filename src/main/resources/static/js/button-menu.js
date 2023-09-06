ed4542ac7f9c26b89db11608bbb020ea4a8f6c4f
Matvey Postnikov <ostnikovnorav@gmail.com>
2023-09-06 17:23:50 +0300

    add button menu

---
+++ src/main/resources/static/js/button-menu.js
@@ -0,0 +1,46 @@
+var toggleButton = document.getElementById("toggleButton");
+var modal = document.getElementById("myModal");
+var closeBtn = document.querySelector(".close");
+const icon = document.getElementById("icon");
+let isOpen = false;
+
+function updateModalSize() {
+    const modalContent = document.querySelector(".modal-content-two");
+    const newWidth = window.innerWidth * 0.23;
+    const newHeight = window.innerHeight * 0.22;
+    const leftOffset = 70;
+    modalContent.style.left = leftOffset + "px";
+    modalContent.style.maxWidth = newWidth + "px";
+    modalContent.style.height = newHeight + "px";
+}
+
+window.addEventListener("load", updateModalSize);
+window.addEventListener("resize", updateModalSize);
+
+toggleButton.addEventListener("click", () => {
+    if (isOpen) {
+        modal.classList.remove("active");
+    } else {
+        modal.classList.add("active");
+    }
+    isOpen = !isOpen;
+    if (isOpen) {
+        icon.style.transform = "rotate(90deg) scale(1.2)";
+    } else {
+        icon.style.transform = "none";
+    }
+});
+
+closeBtn.addEventListener("click", function() {
+    modal.classList.remove("active");
+    isOpen = false;
+    icon.style.transform = "none";
+});
+
+modal.addEventListener("click", function(event) {
+    if (event.target === modal && isOpen) {
+        modal.classList.remove("active");
+        isOpen = false;
+        icon.style.transform = "none";
+    }
+});
